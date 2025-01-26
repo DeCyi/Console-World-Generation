bd920e6c9cc2e10217f86503f320104d820bf903
DeCyi <98708076+DeCyi@users.noreply.github.com>
2025-01-27 00:22:57 +0800

    Changed the folder

--- Main/SampleGraphics.cpp
+++ Main/SampleGraphics.cpp
@@ -7,15 +7,11 @@ void generateWorld();
 int main()
 {
     srand(static_cast<unsigned>(time(0)));
-  // Simulate how a world generated ?:
-
+    // Simulate how a world generated 
     for (int i = 0; i < 30; i++) {
         generateWorld();
     }
-    
-    
 }
-
  void generateWorld() {
         int grap[] = { 1,2,3 };
 
@@ -23,7 +19,6 @@ int main()
         for (int i = 0; i < randomArea; i++) {
             int randomNum = rand() % 3;
             //std::cout << grap[randomNum] << " ";    
-
 
             //std::cout << randomArea;
 
