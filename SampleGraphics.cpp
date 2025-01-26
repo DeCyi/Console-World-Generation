dce413314e91fada3ff89baa7005f452b0b60bea
DeCyi <98708076+DeCyi@users.noreply.github.com>
2025-01-27 00:01:54 +0800

    Add project files.

---
+++ SampleGraphics.cpp
@@ -0,0 +1,35 @@
+
+
+#include <iostream>
+#include <cstdlib>
+ 
+void generateWorld();
+int main()
+{
+    srand(static_cast<unsigned>(time(0)));
+  // Simulate how a world generated ?:
+
+    for (int i = 0; i < 30; i++) {
+        generateWorld();
+    }
+    
+    
+}
+
+ void generateWorld() {
+        int grap[] = { 1,2,3 };
+
+        int randomArea = rand() % 30 + 1;
+        for (int i = 0; i < randomArea; i++) {
+            int randomNum = rand() % 3;
+            //std::cout << grap[randomNum] << " ";    
+
+
+            //std::cout << randomArea;
+
+            std::cout << grap[randomNum] << " ";
+        } // This ain't it
+        std::cout << std::endl;
+    }
+
+
